bf7d06d30fc7f47b073190dde85211243e29c49f
Jacky2737 <jackycheuk07@gmail.com>
2026-03-15 03:06:01 +0800

    add validations and associations tests for Booking and Venue models

--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Venue, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      venue = build(:venue)
+      expect(venue).to be_valid
+    end
+
+    it 'is invalid without a name' do
+      venue = build(:venue, name: nil)
+      expect(venue).not_to be_valid
+      expect(venue.errors[:name]).to include("can't be blank")
+    end
+  end
+
+  describe 'associations' do
+    it 'has many bookings' do
+      association = Venue.reflect_on_association(:bookings)
+      expect(association.macro).to eq(:has_many)
+    end
+
+    it 'destroys dependent bookings' do
+      venue = create(:venue)
+      create(:booking, venue: venue)
+      
+      expect { venue.destroy }.to change { Booking.count }.by(-1)
+    end
+  end
 end
+
