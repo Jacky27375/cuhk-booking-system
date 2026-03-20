4b7a234886c9a9e529d4bd1f61fb92451fb2a149
RiskyPork <joejoecheung2000@gmail.com>
2026-03-21 01:47:08 +0800

    fix(bdd): align venue form and user-booking association for scenario stability

--- features/step_definitions/venue_booking_steps.rb
+++ features/step_definitions/venue_booking_steps.rb
@@ -11,9 +11,13 @@ Given('there is a user {string} with role {string}') do |email, role|
 end
 
 Given('I am logged in as {string}') do |email|
+  user = User.find_by!(email: email)
+  password = ["password", "password1", "Password1!"].find { |value| user.authenticate(value) }
+  raise "No valid password found for #{email}" unless password
+
   visit login_path
   fill_in 'Email', with: email
-  fill_in 'Password', with: 'password'
+  fill_in 'Password', with: password
   click_button 'Log in'
 end
 
@@ -26,7 +30,7 @@ Then('I should not see {string}') do |text|
 end
 
 Given('there is a venue {string}') do |name|
-  create(:venue, name: name)
+  create(:venue, name: name, department: "Science Faculty")
 end
 
 When('I visit the bookings page') do
