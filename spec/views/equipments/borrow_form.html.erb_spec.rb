1f90be060402edaebf9ba4f3412535f13ea3f702
Mona Lisa <24191781+tylerjlcy@users.noreply.github.com>
2026-04-14 01:05:20 +0800

    Update borrow form view spec for date constraint controller

--- spec/views/equipments/borrow_form.html.erb_spec.rb
+++ spec/views/equipments/borrow_form.html.erb_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe 'equipments/borrow_form', type: :view do
     assign(:booking, EquipmentBooking.new(equipment: equipment))
   end
 
-  it 'renders borrow form with minimum dates' do
+  it 'renders borrow form with date constraints' do
     render
 
     expected_min = 5.days.from_now.to_date.to_s
 
-    assert_select 'form' do
-      assert_select 'input[name=?][min=?]', 'booking[start_date]', expected_min
-      assert_select 'input[name=?][min=?]', 'booking[end_date]', expected_min
+    assert_select "form[data-controller='booking-date-range']" do
+      assert_select "input[name='booking[start_date]'][min='#{expected_min}'][data-booking-date-range-target='startDate'][data-action*='booking-date-range#syncEndDateMinimum']"
+      assert_select "input[name='booking[end_date]'][min='#{expected_min}'][data-booking-date-range-target='endDate']"
     end
   end
 end
